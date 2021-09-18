045173a91f87e2e2051fe900942fc183669e5a66
Damango <37273048+Damango@users.noreply.github.com>
2021-09-17 23:20:32 -0400

    Color and style changes

--- LeagueMatchCard.js
+++ LeagueMatchCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import "./LeagueMatchCard.css"
 
 const LeagueMatchCard = (props) => {
-    return ( <div className="match-card-wrapper">
+    return (<div className="match-card-wrapper">
         <div className="match-card-tabs-container">
             <div className="match-type-container card-tab">Ranked Solo</div>
-            <div className="victory-loss-container card-tab victory" style={{borderRight:'1px solid black'}}>Victory</div>
+            <div className="victory-loss-container card-tab victory" style={{ borderRight: '1px solid black' }}>Victory</div>
         </div>
         <div className="match-card-container">
             <div className="icon-rank-container card-data-container">
@@ -14,10 +14,10 @@ const LeagueMatchCard = (props) => {
             </div>
             <div className="summoners-lp-container card-data-container">
                 <div className="summoners-container">
-                    <div className="summoner"></div>
-                    <div className="summoner"></div>
-                    <div className="summoner"></div>
-                    <div className="summoner"></div>
+                    <div className="summoner" style={{ left: '0px', top: '0px', position: 'absolute' }}></div>
+                    <div className="summoner" style={{ right: '0px', top: '0px', position: 'absolute' }}></div>
+                    <div className="summoner" style={{ left: '0px', bottom: '0px', position: 'absolute' }}></div>
+                    <div className="summoner" style={{ right: '0px', bottom: '0px', position: 'absolute' }}></div>
                 </div>
                 <div className="lp-container">+23 LP</div>
             </div>
@@ -27,7 +27,7 @@ const LeagueMatchCard = (props) => {
             </div>
             <div className="cs-gold-container card-data-container">
                 <div className="cs-container">313 (9.8) CS</div>
-                <div className="gold-container">19,000</div>
+                <div className="gold-container"><i class="fas fa-coins"></i>19,000</div>
             </div>
             <div className="items-container card-data-container">
                 <div className="items-icon-wrapper">
@@ -40,7 +40,7 @@ const LeagueMatchCard = (props) => {
                     <div className="item-icon"></div>
                     <div className="item-icon"></div>
                 </div>
-               
+
 
             </div>
             <div className="players-container card-data-container">
@@ -88,11 +88,11 @@ const LeagueMatchCard = (props) => {
                         <div className="username">SADE</div>
                     </div>
                 </div>
-              
-               
+
+
             </div>
         </div>
-    </div> );
+    </div>);
 }
- 
+
 export default LeagueMatchCard;
